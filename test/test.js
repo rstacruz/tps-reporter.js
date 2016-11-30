c35e5ba714ef9a186ed755b7e40863f5a66e5423
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-11-30 16:53:41 +0800

    Points and inferrence

--- test/test.js
+++ test/test.js
@@ -7,27 +7,72 @@ test('points, inferrence', t => {
   result = parse('- task1\n- task2')
   t.deepEqual(result.points, 2)
   t.deepEqual(result.progress, 0)
-
   t.end()
 })
 
 test('points, default', t => {
   result = parse('- task1')
   t.deepEqual(result.children[0].points, 1)
-
   t.end()
 })
 
 test('points, overriding', t => {
   result = parse('- task1 @3pts')
   t.deepEqual(result.children[0].points, 3)
-
   t.end()
 })
 
 test('points, overriding and inferrence', t => {
   result = parse('- task1 @3pts\n- task2')
   t.deepEqual(result.points, 4)
+  t.end()
+})
+
+test('progress, done', t => {
+  result = parse('- task1 @done')
+  t.deepEqual(result.children[0].progress, 1)
+  t.end()
+})
+
+test('progress, in progress', t => {
+  result = parse('- task1 @in_progress')
+  t.deepEqual(result.children[0].progress, 0.5)
+  t.end()
+})
+
+test('progress, percentage', t => {
+  result = parse('- task1 @20%')
+  t.deepEqual(result.children[0].progress, 0.2)
+  t.end()
+})
+
+test('progress, inferrence (0%)', t => {
+  result = parse('- task1\n- task2')
+  t.deepEqual(result.points, 2)
+  t.deepEqual(result.progress, 0)
+
+  t.end()
+})
+
+test('progress, inferrence (50%)', t => {
+  result = parse('- task1 @done\n- task2')
+  t.deepEqual(result.points, 2)
+  t.deepEqual(result.progress, 0.5)
+
+  t.end()
+})
+
+test('progress, inferrence (25%)', t => {
+  result = parse('- task1 @50%\n- task2')
+  t.deepEqual(result.points, 2)
+  t.deepEqual(result.progress, 0.25)
+
+  t.end()
+})
+
+test('progress, inferrence with points', t => {
+  result = parse('- task1 @50% @8pts\n- task2')
+  t.deepEqual(result.progress, 4 / 9)
 
   t.end()
 })
